e35fcbb400f02ef81a6dedeb6a71a9df36facf36
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-02-28 19:58:06 +0100

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,74 +1,87 @@
-public class Worker{
-public class Company {
-  // Możesz dodać właściwości i metody dla Company
+public class Worker {
+    public class Company {
+        public void companyInfo() {
+            System.out.println("This is a company.");
+        }
+    }
+
+    public class WorkerOfComp extends Company {
+        public void work() {
+            System.out.println("Worker is working in the company.");
+        }
+    }
+
+    public class ItWorker extends WorkerOfComp implements IBasicSkills, ItSkills {
+        @Override
+        public void coding() {
+            System.out.println("IT Worker is coding.");
+        }
+
+        public void programming() {
+            System.out.println("IT Worker is programming.");
+        }
+
+        @Override
+        public void writeAlphabet() {
+            System.out.println("IT Worker is writing the alphabet.");
+        }
+
+        @Override
+        public void readAlphabet() {
+            System.out.println("IT Worker is reading the alphabet.");
+        }
+    }
+
+    public class Programmer implements AlgorythmicSkills {
+        @Override
+        public void algorytmBubleSort() {
+            System.out.println("Programmer is implementing Bubble Sort algorithm.");
+        }
+
+        @Override
+        public void coding() {
+            System.out.println("Programmer is coding.");
+        }
+    }
+
+    public interface AlgorythmicSkills extends ItSkills {
+        void algorytmBubleSort();
+    }
+
+    public interface ItSkills {
+        void coding();
+    }
+
+    public interface IBasicSkills {
+        void writeAlphabet();
+        void readAlphabet();
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Worker workerClass = new Worker(); 
+
+        Company company = workerClass.new Company();
+        company.companyInfo();
+
+        WorkerOfComp worker = workerClass.new WorkerOfComp();
+        worker.companyInfo();
+        worker.work();
+
+        ItWorker itWorker = workerClass.new ItWorker();
+        itWorker.companyInfo();
+        itWorker.work();
+        itWorker.coding();
+        itWorker.programming();
+        itWorker.writeAlphabet();
+        itWorker.readAlphabet();
+
+        Programmer programmer = workerClass.new Programmer();
+        programmer.coding();
+        programmer.algorytmBubleSort();
+    }
 }
 
-public class WorkerOfComp extends Company {
-  // Możesz dodać właściwości i metody dla Worker
-}
-
-
-public class ItWorker extends WorkerOfComp implements IBasicSkills, ItSkills {
-
-
-  @Override
-
-  public void coding()
-  {
-
-  }
-
-  public void programming() {
-      // Implementacja metody
-  }
-
-  @Override
-  public void writeAlphabet() {
-      // Implementacja metody interfejsu
-  }
-
-  @Override
-  public void readAlphabet() {
-      // Implementacja metody interfejsu
-  }
-}
-
-public class Programmer implements algorytmicSkilss
-{
-
-@Override
-public void algorytmBubleSort(){};
-@Override
-public void coding(){};
-
-}
-
-
-public interface algorytmicSkilss  extends ItSkills
-{
-  public void algorytmBubleSort();
-  
-}
-
-public interface ItSkills {
-
-  public void coding();
-
-  
-}
-
-public interface IBasicSkills {
-  void writeAlphabet();
-  void readAlphabet();
-}
-
-public interface IInnerMain {
-  // Możesz dodać metody dla interfejsu
-}
-
-public class Main {
-  public static void main(String[] args) {
-      System.out.println("Hello World");
-  }
-}
-}
